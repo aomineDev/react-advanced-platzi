318fbe2f8a1872720ac027364be27f49d2dd16ae
aomine <xomarxpl07@outlook.es>
2019-09-03 12:42:52 -0500

    session estorage react context hook

--- petgram-platzi/src/pages/NotRegisteredUser.jsx
+++ petgram-platzi/src/pages/NotRegisteredUser.jsx
@@ -1,50 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 
-import Context from '../Context'
+import { Context } from '../Context'
 import { registerMutation } from '../graphql/mutations/RegisterMutation'
 import { loginMutation } from '../graphql/mutations/LoginMutation'
 
 import { UserForm } from '../components/UserForm'
 
 export const NotRegisteredUser = () => {
+  const { activateAuth } = useContext(Context)
   const [isLogin, setIsLogin] = useState(true)
+
   const [register, { loadingSignIn, errorSignIn }] = registerMutation()
   const [login, { loadingLogIn, errorLogIn }] = loginMutation()
 
+  const onSignIn = (email, password) => {
+    const input = { email, password }
+    const variables = { input }
+    register({ variables })
+      .then(({ data }) => {
+        const { signup } = data
+        activateAuth(signup)
+      })
+  }
+
+  const onLogIn = (email, password) => {
+    const input = { email, password }
+    const variables = { input }
+    login({ variables })
+      .then(({ data }) => {
+        const { login } = data
+        activateAuth(login)
+      })
+  }
+
+  const errorMsgLogIn = errorLogIn && 'Email y/o contraseña incorrecta.'
+  const errorMsgSignIn = errorSignIn && 'El email ya existe o hay algún problema.'
+
   return (
-    <Context.Consumer>
-      {
-        ({ activateAuth }) => {
-          const onSignIn = (email, password) => {
-            const input = { email, password }
-            const variables = { input }
-            register({ variables })
-              .then(activateAuth)
-          }
-
-          const onLogIn = (email, password) => {
-            const input = { email, password }
-            const variables = { input }
-            login({ variables })
-              .then(activateAuth)
-          }
-
-          const errorMsgLogIn = errorLogIn && 'Email y/o contraseña incorrecta.'
-          const errorMsgSignIn = errorSignIn && 'El email ya existe o hay algún problema.'
-
-          return (
-            <UserForm
-              disabled={loadingSignIn || loadingLogIn}
-              errorLogIn={errorMsgLogIn}
-              errorSignIn={errorMsgSignIn}
-              onLogIn={onLogIn}
-              onSignIn={onSignIn}
-              isLogin={isLogin}
-              handleClick={() => { setIsLogin(!isLogin) }}
-            />
-          )
-        }
-      }
-    </Context.Consumer>
+    <UserForm
+      disabled={loadingSignIn || loadingLogIn}
+      errorLogIn={errorMsgLogIn}
+      errorSignIn={errorMsgSignIn}
+      onLogIn={onLogIn}
+      onSignIn={onSignIn}
+      isLogin={isLogin}
+      handleClick={() => { setIsLogin(!isLogin) }}
+    />
   )
 }
